a1cdd6dfe11f0544b48b67fdfe92b80fa8201da9
mr-arasu <perarasu020405@gmail.com>
2025-08-06 11:57:38 +0530

    new sql file

--- test.java
+++ test.java
@@ -1,16 +1,6 @@
 public class test {
-	static int x= 10;
-
-	static class Inner{
-		void show() {
-			System.out.println(" Static value x ="+x);
-		}
-	}
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		Inner obj= new Inner ();
-		obj.show();
-	}
-
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        System.out.println("Hello World");
+    }
 }
